5fdc54395b982d268eddf594a79adcf8bd491f11
Yomaira Escano <yjescano@gmail.com>
2019-02-14 21:05:36 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,26 +1,20 @@
 // declare your functions here...
 function paragraphSelector() {
-<<<<<<< HEAD
-  return $('p')
+  return $('p');
 }
 
 function lastImageSelector() {
-  return $('img:last')
+  return $('img:last');
 }
 
 function ninjaBabySelector() {
-  return $('#baby-ninja')
+  return $('#baby-ninja');
 }
 
 function divSelector() {
-  return $('.pics')
+  return $('.pics');
 }
 
 function firstListItem() {
-  return $('#pic-list li:first-child')
-=======
-  $(document).ready(function() {
-    $('p')
-  })
->>>>>>> 467aee333920c06ff089442adf3fbece55a97fa7
+  return $('#pic-list li:first-child');
 }
